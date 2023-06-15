9dff6662b3e73a9b5bcd16e6aa963e3777d94c92
Gabriel Resende <gabriellrr2005@gmail.com>
2023-06-15 16:44:22 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,6 +23,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller findByAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 
 }
