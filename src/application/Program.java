c2d12b6e58c25a020921a2bd687b353574d7a861
Gabriel Resende <gabriellrr2005@gmail.com>
2023-06-15 15:24:05 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,7 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		System.out.println(seller);
